02640a4c11b8ed072b57b9992a72c63300dc73d2
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-03-25 22:57:59 +0530

    security fixes

--- breakfast/brainfuck-transpiler/next.config.mjs
+++ breakfast/brainfuck-transpiler/next.config.mjs
@@ -22,6 +22,56 @@ const nextConfig = {
     parallelServerCompiles: true,
   },
   serverExternalPackages: ['breakfast_bg.wasm'],
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+          },
+        ],
+      },
+      {
+        source: '/wasm/(.*)',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/wasm',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config, { isServer }) => {
     // Enable WebAssembly support
     config.experiments = {
